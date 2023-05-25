1671bbc5408f824e39e9edc9247b78bfbdf5a2e7
강준성 <jskang@wisenut.co.kr>
2023-05-25 10:19:11 +0900

    feat: 사이드메뉴 숨김 기능 추가

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,14 @@
   <div>
     <div class="main-body">
       <div class="side-menu">
-       <SideMenu v-model:menuItems="sideMenu" />
+       <SideMenu v-model:menuItems="sideMenu" :side-menu-active="sideMenuActive" :width="sideWidth"/>
+        <div style="position: absolute; bottom: 25px; z-index: 10;">
+          <va-icon name="menu" :size="'3em'" v-if="!sideMenuActive" style="line-height: 0px; height: 0px;" v-on:click="sideMenuActiveOnOff"></va-icon>
+          <va-icon name="menu_open" :size="'3em'" v-if="sideMenuActive" style="line-height: 0px; height: 0px;" v-on:click="sideMenuActiveOnOff"></va-icon>
+        </div>
       </div>
-      <div class="content body-height">
-        <NavBar :userId="userId" :userName="userName"/>
+      <div class="content body-height" :style="contentStyle">
+        <NavBar :userId="userId.value" :userName="userName.value"/>
         <div class="container body-height">
           <slot />
         </div>
@@ -20,14 +24,51 @@
 
 import { sideMenuData } from '~~/assets/data/sidemenu';
 const sideMenu = ref(sideMenuData);
+const sideMenuActive = ref(true);
 
-const userId = ref('admin');
-const userName = ref('관리자');
+const sideWidth = ref("250px");
+const sideStyle = ref("width: 250px;");
+const contentStyle = ref("width: calc( 100% - 250px);");
 
+const userId: globalThis.Ref<string> = ref('admin');
+const userName: globalThis.Ref<string> = ref('관리자');
 
 useHead({
   title: '기본페이지'
 })
+
+const sideMenuActiveOnOff = () => {
+  let style = "";
+  let logoTitle: any = document.getElementsByClassName("va-list-item-label");
+  let menuTitle: any = document.getElementsByClassName("va-sidebar__title va-sidebar-item-title");
+
+  sideMenuActive.value = !sideMenuActive.value;
+  // 사이드메뉴 펼쳐질 때
+  if (sideMenuActive.value) {
+    sideWidth.value = "270px";
+    sideStyle.value = "width: 270px;";
+    contentStyle.value = "width: calc( 100% - 270px);";
+  }
+  // 사이드메뉴 접을 때
+  else {
+    sideWidth.value = "80px";
+    sideStyle.value = "width: 80px;";
+    contentStyle.value = "width: calc( 100% - 80px);";
+    style = "display: none;";
+  }
+
+  for (let i in menuTitle) {
+    if (menuTitle[i]["style"]) {
+      menuTitle[i]["style"] = style;
+    }
+  }
+
+  for (let i in logoTitle) {
+    if (logoTitle[i]["style"]) {
+      logoTitle[i]["style"] = style;
+    }
+  }
+}
 </script>
 
 <style scoped>
