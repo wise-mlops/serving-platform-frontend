07eab24bb36880ff3b32d56f32659c68bc450db3
조미곤 <migon@wisenut.co.kr>
2024-02-21 17:51:13 +0900

    feat: 상세보기 페이지 기능 추가

--- composables/columns.ts
+++ composables/columns.ts
@@ -6,3 +6,10 @@ export const servicesCol = ref([
   { label: 'Test', key: 'test', sortable: false, width: '5%' },
   { label: 'Remove', key: 'remove', sortable: false, width: '5%' }
 ])
+
+export const overviewCol = ref([
+  { label: 'Status', key: 'status', width: '4%' },
+  { label: 'Type', key: 'type', width: '11%' },
+  { label: 'Last Transition Time', key: 'lastTransitionTime', width: '11%' },
+  { label: 'Serverity', key: 'severity', width: '8%' },
+])
